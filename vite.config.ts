0720c3c573c5996af847c73df3ff3d0ee05e233f
monir-shokohyan <monir-shokohyan@users.noreply.github.com>
2025-01-29 11:27:30 +0000

    deploy: 2259ac805283132590fdd440067729ea5e07a486

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import prettier from 'vite-plugin-prettier'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsConfigPaths(), prettier()],
-})
